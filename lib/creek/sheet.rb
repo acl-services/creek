114dab7dc2f2a2a46d774de3050ed1c3c27a8663
Ilyas Garaev <ilyas.garaev@flatstack.com>
2016-04-18 13:40:42 +0300

    fix converter options

--- lib/creek/sheet.rb
+++ lib/creek/sheet.rb
@@ -80,19 +80,19 @@ module Creek
     end
 
     def converter_options(cell_name, style_idx)
-      @_converter_options ||= begin
-        options = { :shared_strings => book.shared_strings.dictionary }
-        options.merge!(html_options(cell_name, style_idx)) if book.options[:with_html]
-        options
+      @_default_converter_options ||= begin
+        { :shared_strings => book.shared_strings.dictionary }
       end
+
+      book.options[:with_html] ?  html_options(cell_name, style_idx) : @_default_converter_options
     end
 
     def html_options(cell_name, style_idx)
-      {
+      @_default_converter_options.merge({
         :with_html => true,
         :html_cell => html_cell?(cell_name),
         :cell_style => book.cell_styles[style_idx.to_i]
-      }
+      })
     end
 
     def html_cell?(cell_name)
